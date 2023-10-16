a9096adabae52dd7bfd7346195b580a40e8b52e7
zyh <yuhe.zheng@zenjoy.net>
2023-10-16 21:05:05 +0800

    + add：game play pannel test

---
+++ assets/demo-play/Main.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+import {Root} from "db://oops-framework/core/Root";
+const { ccclass, property } = _decorator;
+
+@ccclass('Main')
+export class Main extends Root {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
